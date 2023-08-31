35d91cad803a73c3181027902af1304820c57723
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-08-31 14:04:06 -0300

    correcao

--- tudao/ex37.py
+++ tudao/ex37.py
@@ -1,6 +1,6 @@
-valor_casa = float(input('Qual o valor da casa? '))
-salario_comprador = float(input('Qual o seu salario? '))
-anos = float(input('Em quantos anos vc pretende pagar? '))
+valor_casa = float(input(' valor da casa? R$'))
+salario_comprador = float(input('salario do comprador?R$ '))
+anos = int(input('Em quantos anos vc pretende pagar? '))
 
 prestacao_maxima = salario_comprador * 0.3
 meses = anos * 12
@@ -8,6 +8,7 @@ meses = anos * 12
 prestacaocasa = valor_casa / meses
 
 if prestacaocasa > prestacao_maxima:
-    print('Desculpe nao podemos aprovar seu emprestimo!')
+    print('Para pagar a casa de R${:.2f} em {} anos a prestacao sera de R${:.2f}'.format(valor_casa,anos,prestacaocasa))
+    print('Emprestimo NEGADO')
 else:
     print('Seu emprestimo foi aprovado, PARABENS. O valor da parcela ficou {:.2f} por {} anos!'.format(prestacaocasa,anos))
--- tudao/ex38.py
+++ tudao/ex38.py
@@ -1,22 +1,26 @@
 import time
 num = int(input('Diga um numero qualquer: '))
 
+conversao = int(input('''Escolha uma das bases para conversao: 
+[ 1 ] converter para BINARIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL
+Sua opcao: '''))
+
 print('-=-'*20)
 print('                       Processando...')
 print('-=-'*20)
 
 time.sleep(2)
 
-conversao = int(input('Digite 1 para binario, 2 para octal ou 3 para hexadecimal: '))
-
 if conversao == 1:
     binario = bin(num)
-    print('Seu numero em binario ficou >> {} <<'.format(binario))
+    print('Seu numero em binario ficou >> {} <<'.format(binario)[2:])
 elif conversao == 2:
     octal = oct(num)
-    print('Seu numero em octal ficou >> {} <<'.format(octal))
+    print('Seu numero em octal ficou >> {} <<'.format(octal)[2:])
 elif conversao == 3:
     hexadecimal = hex(num)
-    print('Seu numero em hexadecimal ficou >> {} <<'.format(hexadecimal))
+    print('Seu numero em hexadecimal ficou >> {} <<'.format(hexadecimal)[2:])
 else:
     print('Numero digitado esta incorreto!')
