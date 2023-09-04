36dfdf40dc6d784493480f198de8be9bab043e2e
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-09-04 19:55:33 -0300

    att ex46

--- tudao/ex46.py
+++ tudao/ex46.py
@@ -1,4 +1,86 @@
-import random
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0,2)
+print('''Suas opcoes:
+[ 0 ] Pedra
+[ 1 ] papel
+[ 2 ] tesoura''')
+jogador =  int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print("PO!!")
+sleep(1)
+print('=-'*11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('=-'*11)
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print("JOGADOR VENCE")
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('Jogada invalida')
+
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUATDOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada invalida')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""import random
 print('=-='*20)
 print('consegue ganhar de mim no pedra papel e tesoura?')
 print('                    Escolhe um:')
@@ -23,5 +105,6 @@ elif ppt.lower() == 'tesoura' and escolha_pc == 'papel':
     print('Eu escolhi {}, Voce ganhou haha !'.format(escolha_pc))
 
 elif ppt.lower() == escolha_pc:
-    print('Eu escolhi {}, Empatamos !'.format(escolha_pc))
+    print('Eu escolhi {}, Empatamos !'.format(escolha_pc))"""
+
 
