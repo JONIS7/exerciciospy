eee3539851a3c35308211880efa741af8af02627
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-09-04 20:54:07 -0300

    add 47.48

---
+++ tudao/ex47.py
@@ -0,0 +1,8 @@
+from time import sleep
+print('O ano esta virando!')
+print('CONTAGEM REGRESSIVA!')
+for c in range(10, 0, -1 ):
+    sleep(1)
+    print(c)
+sleep(1)
+print('FELIZ ANO NOVO !!')
---
+++ tudao/ex48.py
@@ -0,0 +1,7 @@
+print('=-'*20)
+print('AQUI ESTAO OS NUMERO PARES DE 1 ATE 50!')
+print('=-'*20)
+
+for c in range(2,51,2):
+    print(c)
+print('FIM')
