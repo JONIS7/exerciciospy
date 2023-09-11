b05b7fd8257f3b3e4f52a1e1ddd2b3ccc66ba3a8
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-09-10 23:37:36 -0300

    att 57

--- tudao/ex57.py
+++ tudao/ex57.py
@@ -3,10 +3,11 @@ h_mais_velho_nome = None
 h_mais_velho_idade = float('-inf')
 mulheres_menoresde20 = 0
 
-for c in range(4):
-    nome = input('Qual seu nome: ')
+for c in range(1 , 5):
+    print('----- {}* PESSOA ----- '.format(c))
+    nome = str(input('Qual seu nome: ')).strip()
     idade = int(input('Qual sua idade: '))
-    sexo = input('Qual seu sexo (M/F): ')
+    sexo = input('Qual seu sexo (M/F): ').strip()
     print('PROXIMO!')
 
     idades.append(idade)
@@ -20,13 +21,13 @@ for c in range(4):
 
 media_idade = sum(idades) / 4
 
-print('A média de idade das pessoas é: {:.2f}'.format(media_idade))
+print('A média de idade das pessoas é{:.1f} anos'.format(media_idade))
 if h_mais_velho_nome:
-    print('O homem mais velho é:', h_mais_velho_nome)
+    print('O homem mais velho tem {} anos e se chama {}'.format(h_mais_velho_idade, h_mais_velho_nome))
 else:
     print('Não há homens na lista.')
 
-print('O número de mulheres com menos de 20 anos é:', mulheres_menoresde20)
+print('Ao todo sao {} mulheres com menos de 20 anos'.format(mulheres_menoresde20))
 
 
 
