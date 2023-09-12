98dd9261b31c4d14d23ae36389c338e0893a6ae7
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-09-12 19:12:41 -0300

    att

---
+++ tudao/ex58.py
@@ -0,0 +1,5 @@
+sexo = str(input('Qual seu sexo?[M/F]: ')).upper()
+while sexo != 'M' and sexo != 'F':
+        print('valor digitado incorreto, porfavor digite M ou F!!')
+        sexo = str(input('Qual seu sexo?[M/F]: ')).upper()
+print('FIm')
