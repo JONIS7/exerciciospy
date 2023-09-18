8e25b25a2bd4d49ad37c91df49be54710fffc98c
Jonathan Willian <JONISEHIS7@GMAIL.COM>
2023-09-17 21:52:43 -0300

    att

--- tudao/ex66.py
+++ tudao/ex66.py
@@ -1,12 +1,24 @@
 num = int(input('Digite um numero: '))
 lista_num = []
+num_digitados = 0
 maior_num = float('-inf')
 menor_num = float('inf')
 
-while num != 999:
+while num != 10000000000:
     lista_num.append(num)
+    num_digitados += 1
     if num > maior_num:
         maior_num = num
     if num < menor_num:
         menor_num = num
-    media = 
+    continuar = str(input('Deseja continuar [S/N]: ')).strip()
+    if continuar.upper() == 'S':
+        num = int(input('Digite um numero: '))
+    else:
+        print('PROGAMA FINALIZADO')
+        break
+soma = sum(lista_num)
+media = soma / num_digitados
+print('A media entre os numeros {:.2f}'.format(media))
+print('O maior numero é {}'.format(maior_num))
+print('O menor numero é {}'.format(menor_num))
